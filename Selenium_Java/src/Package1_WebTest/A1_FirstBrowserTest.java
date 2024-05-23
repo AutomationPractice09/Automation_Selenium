e278fbe7932a5d9221c7e1418197cc2d6baacbc5
AutomationPractice09 <digvipatil1111@gmail.com>
2024-05-23 22:54:45 +0530

    This is first commit

---
+++ Selenium_Java/src/Package1_WebTest/A1_FirstBrowserTest.java
@@ -0,0 +1,20 @@
+package Package1_WebTest;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class A1_FirstBrowserTest {
+
+	public static void main(String[] args) 
+	{
+	
+//		WebDriver driver = new ChromeDriver();      //it will instantiate chrome browser
+		
+//		WebDriver driver1 = new EdgeDriver();       //it will instantiate edge browser
+		
+		WebDriver driver2 = new FirefoxDriver();    //it will instantiate FireFox browser
+		
+	}
+
+}
---
+++ Selenium_MavenArchetype/src/test/java/package1_2305/A1_BrowserLaunch.java
@@ -0,0 +1,16 @@
+package package1_2305;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class A1_BrowserLaunch {
+
+	public static void main(String[] args) 
+	{
+		
+		WebDriver driver1 = new ChromeDriver();
+		driver1.get("https://google.com");
+	
+	}
+
+}
